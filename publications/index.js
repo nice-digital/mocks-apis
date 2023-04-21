06411c4a7865c8dad6d423a26a1af8eeeba20ac8
Ian Routledge <ian.routledge@nice.org.uk>
2023-04-21 15:18:43 +0100

    NXT-153 Create basic mock APIs for indicators (SSE and publications) (#10)
    * NXT-153 Add mock indicator endpoints
    
    * NXT-153 Revert to port 80 for mock search API
    
    * NXT-153 Add mock publications
    
    * NXT-153 Remove NA project from sample indicator
    
    ---------
    
    Co-authored-by: Chris Barker <chris.barker@nice.org.uk>

--- publications/index.js
+++ publications/index.js
@@ -3,5 +3,5 @@ const app = require("./app.js");
 const port = process.env.PORT || 80;
 
 app.listen(port, () => {
-  console.log(`Mock search api running at http://localhost:${port}`);
+  console.log(`Mock Publications running at http://localhost:${port}`);
 });
